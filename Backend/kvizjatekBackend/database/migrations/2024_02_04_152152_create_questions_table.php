a7cda0a15c9a171efb2bc49148ba59e92eaa4e84
Tari Attila <tari.attila@expedit.hu>
2024-02-04 16:41:17 +0100

    all table migration created

---
+++ Backend/kvizjatekBackend/database/migrations/2024_02_04_152152_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('questiontext');
+            $table->unsignedBigInteger('topicid');
+            $table->string('answer1');
+            $table->string('answer2');
+            $table->string('answer3');
+            $table->string('answer4');
+            $table->integer('correctanswer');
+        });
+        Schema::table('questions', function (Blueprint $table){
+            $table->foreign('topicid')->references('id')->on('topic');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
---
+++ Backend/kvizjatekBackend/database/migrations/2024_02_04_153749_create_userboost_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('userboost', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('booster1');
+            $table->boolean('booster2');
+            $table->boolean('booster3');
+            $table->timestamps();
+        });
+        Schema::table('userboost', function(Blueprint $table){
+            $table->foreign('id')->references('id')->on('players');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('userboost');
+    }
+};
