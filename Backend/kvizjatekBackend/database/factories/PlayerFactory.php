3a1fda389705e1e4f97b60723adeeea34ed85376
Tari Attila <tari.attila@expedit.hu>
2024-02-04 16:14:35 +0100

    players seeder creation, faker locale set to hu_HU, player factory creation

--- Backend/kvizjatekBackend/database/factories/PlayerFactory.php
+++ Backend/kvizjatekBackend/database/factories/PlayerFactory.php
@@ -3,12 +3,16 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Player>
  */
 class PlayerFactory extends Factory
 {
+    protected static ?string $password;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,12 @@ class PlayerFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => static::$password ??= Hash::make('password'),
+            'regdate' => now(),
+            'score' => fake()->randomNumber(4,false),
+            'credit' => fake()->randomNumber(3,false)
         ];
     }
 }
