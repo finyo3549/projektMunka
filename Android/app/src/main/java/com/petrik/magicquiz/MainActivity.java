84977e95475a23de8217f9aeda075d4afb345f98
Tari Attila <tari.attila@expedit.hu>
2024-02-19 13:39:30 +0100

    a

--- Android/app/src/main/java/com/petrik/magicquiz/MainActivity.java
+++ Android/app/src/main/java/com/petrik/magicquiz/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
-import com.petrik.magicquiz.ui.login.LoginActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,13 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
     }
     private void init(){
         loginButton = findViewById(R.id.loginButton);
