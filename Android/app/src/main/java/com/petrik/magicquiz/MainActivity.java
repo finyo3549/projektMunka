9f5bdc0af3fd21768b8977f48900e603fddf81cd
Tari Attila <tari.attila@expedit.hu>
2024-02-19 12:15:01 +0100

    activitylogin, colors, themes modification

--- Android/app/src/main/java/com/petrik/magicquiz/MainActivity.java
+++ Android/app/src/main/java/com/petrik/magicquiz/MainActivity.java
@@ -2,13 +2,33 @@ package com.petrik.magicquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.petrik.magicquiz.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;
+    private Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void init(){
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
     }
 }
