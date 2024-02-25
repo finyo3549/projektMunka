b695f8024879f30ca70ce7fc95cad49eb4dcdfba
Klibit <kovacsbence720@gmail.com>
2024-02-25 13:16:58 +0100

    Main Page done. Need to format a bit.

--- WebFrontend/MagicQuizz/src/App.jsx
+++ WebFrontend/MagicQuizz/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Content from './Content'
 
 function App() {
 
   return (
-    <div>
-      <h1>Magic Quizz</h1>
-    </div>
+    <>
+      <Content />
+    </>
   )
 }
 
