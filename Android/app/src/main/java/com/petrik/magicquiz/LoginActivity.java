28cfb2ed9316d76bec8129330e013c1694167af0
Tari Attila <tari.attila@expedit.hu>
2024-03-11 13:47:38 +0100

    deleted Player class

--- Android/app/src/main/java/com/petrik/magicquiz/LoginActivity.java
+++ Android/app/src/main/java/com/petrik/magicquiz/LoginActivity.java
@@ -2,7 +2,9 @@ package com.petrik.magicquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -99,8 +101,11 @@ public class LoginActivity extends AppCompatActivity {
                     try {
                         Map<String, String> responseData = converter.fromJson(responseContent, Map.class);
                         String token = responseData.get("token");
-                        Toast.makeText(LoginActivity.this, token, Toast.LENGTH_SHORT).show();
-
+                        Toast.makeText(LoginActivity.this, "Sikeres bejelentkezés", Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedPreferences = getSharedPreferences("token", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("token", token);
+                        editor.apply();
                         Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
                         startActivity(intent);
                     } catch (JsonSyntaxException e) {
