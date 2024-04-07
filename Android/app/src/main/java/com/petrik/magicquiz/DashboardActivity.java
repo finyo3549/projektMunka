844550020a3d37367bcd85a43beb36216bd6b37b
Tari Attila <tari.attila@expedit.hu>
2024-04-07 10:31:35 +0200

    Debugged Logout

--- Android/app/src/main/java/com/petrik/magicquiz/DashboardActivity.java
+++ Android/app/src/main/java/com/petrik/magicquiz/DashboardActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 
 import android.content.SharedPreferences;
@@ -74,30 +75,34 @@ public class DashboardActivity extends AppCompatActivity implements NavigationVi
         toolbar = findViewById(R.id.toolbar);
         drawerLayout = findViewById(R.id.DrawerLayout);
     }
-    private void logout(){
+
+    private void logout() {
         Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
 
         try {
             SharedPreferences sharedPreferences = getSharedPreferences("userdata", MODE_PRIVATE);
             Logout logout = new Logout(this);
-            logout.logoutUser(new Logout.LogoutListener() {
+            logout.setListener(new Logout.LogoutListener() {
                 @Override
                 public void onUserLoggedOut() {
                     SharedPreferences.Editor editor = sharedPreferences.edit();
                     editor.clear();
                     editor.commit();
-                    Intent intent = new Intent(DashboardActivity.this, MainActivity.class);
+                    Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
                     startActivity(intent);
                 }
             });
+            logout.logoutUser();
         } catch (Exception e) {
             Toast.makeText(this, "Hiba a felhasználói adatok törlésekor!", Toast.LENGTH_SHORT).show();
             //finish();
             //System.exit(0);
-        };
+        }
+        ;
 
     }
-    private void initDrawer(){
+
+    private void initDrawer() {
         NavigationView navigationView = findViewById(R.id.nav_view);
         View headerView = navigationView.getHeaderView(0);
         navHeaderUsername = headerView.findViewById(R.id.nav_header_username);
--- Android/app/src/main/java/com/petrik/magicquiz/Logout.java
+++ Android/app/src/main/java/com/petrik/magicquiz/Logout.java
@@ -8,21 +8,30 @@ import android.os.AsyncTask;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Logout {
     private String url = "http://10.0.2.2:8000/api/logout";
     private Context mContext;
+    private LogoutListener mListener;
 
-    public void logoutUser(final LogoutListener listener) {
+
+    public void logoutUser() {
         SharedPreferences token = mContext.getSharedPreferences("userdata", MODE_PRIVATE);
         String tokenString = token.getString("token", "");
-        RequestTask requestTask = new RequestTask(mContext, url, "POST", tokenString, listener);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer " + tokenString);
+        String RequestParams = "";
+        RequestTask requestTask = new RequestTask(mContext,url, "POST", RequestParams,  headers, mListener);
         requestTask.execute();
     }
 
     public interface LogoutListener {
         void onUserLoggedOut();
+    }
+    public void setListener(LogoutListener listener) {
+        this.mListener = listener;
     }
 
     public Logout(Context context) {
@@ -30,19 +39,21 @@ public class Logout {
     }
 
     private class RequestTask extends AsyncTask<Void, Void, Response> {
+        private final LogoutListener mListener;
         String requestUrl;
         String requestType;
         String requestParams;
         Context mContext;
+        Map<String, String> headers;
 
-        private LogoutListener mListener;
 
-        public RequestTask(Context context, String requestUrl, String requestType, String requestParams, LogoutListener listener) {
+        public RequestTask(Context context, String requestUrl, String requestType, String requestParams, Map<String, String> headers, LogoutListener mListener) {
             this.mContext = context;
             this.requestUrl = requestUrl;
             this.requestType = requestType;
             this.requestParams = requestParams;
-            this.mListener = listener;
+            this.headers = headers;
+            this.mListener = mListener;
         }
 
 
@@ -51,7 +62,7 @@ public class Logout {
             Response response = null;
             try {
                 if (requestType.equals("POST")) {
-                    response = RequestHandler.post(requestUrl, requestParams);
+                    response = RequestHandler.postAuthenticated(requestUrl, requestParams, headers.get("Authorization"));
                 }
             } catch (IOException e) {
 
