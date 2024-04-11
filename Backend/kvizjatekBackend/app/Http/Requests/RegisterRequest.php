ed23901c666bd2f6c01b7f54e2556df7c4ac17fa
Tari Attila <tari.attila@expedit.hu>
2024-04-11 15:51:54 +0200

    modified password complexity

--- Backend/kvizjatekBackend/app/Http/Requests/RegisterRequest.php
+++ Backend/kvizjatekBackend/app/Http/Requests/RegisterRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
 
 class RegisterRequest extends FormRequest
 {
@@ -24,7 +26,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => "required|string",
             'email' => "required|email|unique:users,email",
-            'password' => "required|string|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/",
+            'password' => ['required', Password::min(8)->mixedCase()->numbers()->symbols()->uncompromised()],
             'gender' => 'string'
         ];
     }
