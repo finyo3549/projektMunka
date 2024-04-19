5fe62c4953f624644246e43fee2e1f64ceb5a7c7
Klibit <kovacsbence720@gmail.com>
2024-04-19 20:06:47 +0200

    standarsds css set, callfunction working

--- WebFrontend/MagicQuizzFrontend/src/pages/GamePage.jsx
+++ WebFrontend/MagicQuizzFrontend/src/pages/GamePage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
+import '../standards.css'
 
 const GamePage = () => {
     const [questions, setQuestions] = useState([]);
